740290d871c990c7040a5d0411f94b8c3f3bfec7
Faugnell <vic.petit98@gmail.com>
2024-03-05 15:03:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Hello, World!
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <?php
+    // Code PHP pour afficher Hello World
+    echo "<h1>Hello World!</h1>";
+    ?>
+</body>
+</html>
