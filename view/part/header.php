8de567f0c93249e07dd76afef6c4be66d0eda66e
Faugnell <vic.petit98@gmail.com>
2024-03-06 15:39:34 +0100

    Update header.php

--- view/part/header.php
+++ view/part/header.php
@@ -8,8 +8,8 @@ Autoloader::register();
 <head>
     <meta charset="UTF-8">
     <title>Lab'CESI</title>
-    <link rel="stylesheet" type="text/css" href="<?= SITE_URL; ?>/assets/css/style.css">
-    <script src="<?= SITE_URL; ?>/assets/js/"></script>
+    <link rel="stylesheet" type="text/css" href="https://api-inkventory.ovh/assets/css/style.css">
+    <script src="<?= SITE_URL; ?>/assets/js/script.js"></script>
 </head>
 
 <?php require_once(realpath(__DIR__ . "/menu.php")); ?>
