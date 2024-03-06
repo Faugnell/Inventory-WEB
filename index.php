60d104fb0c2f407e36fcd610761f038cfbfd032d
Faugnell <vic.petit98@gmail.com>
2024-03-06 14:35:40 +0100

    ajout du mvc

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-    <?php
-    // Code PHP pour afficher Hello World
-    echo "<h1>Hello, World!</h1>";
-    echo "<p>ceci est un petit paragraphe</p>"
-    ?>
-</body>
-</html>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$getParams = isset($_GET) ? $_GET : null;
+$postParams = isset($_POST) ? $_POST : null;
+$params = [
+    "get"  => $getParams,
+    "post" => $postParams
+];
+
+if (isset($_GET['user'])) {
+
+}
+
+else if (isset($_GET['admin'])) {
+
+}
+
+else {
+    require_once('./view/home.php');
+}
---
+++ view/home.php
@@ -0,0 +1,4 @@
+<?php
+require_once(realpath(__DIR__ . '/view/part/header.php'));
+echo "<h1>Hello, world!</h1>";
+?>
