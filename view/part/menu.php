8f7e63dbdf09c6cd0ea9b9322e0162f8388f71f3
Faugnell <vic.petit98@gmail.com>
2024-03-06 15:30:08 +0100

    update

---
+++ view/part/menu.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="logo">Mon Logo</div>
+    <nav>
+        <ul>
+            <li><!--<a href="<?= SITE_URL; ?>/projets">-->Projets</a></li>
+            <li><!--<a href="<?= SITE_URL; ?>/equipements">-->Equipements</a></li>
+            <li><!--<a href="<?= SITE_URL; ?>/evenements">-->Evenements</a></li>
+            <li><!--<a href="<?= SITE_URL; ?>/histoire">-->Histoire</a></li>
+        </ul>
+    </nav>
+</header>
