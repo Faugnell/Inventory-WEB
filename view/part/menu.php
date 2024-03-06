e4c2b194864ab94aa4b450cbd4cde54389ec4ffb
Faugnell <vic.petit98@gmail.com>
2024-03-06 15:33:35 +0100

    Update menu.php

--- view/part/menu.php
+++ view/part/menu.php
@@ -2,10 +2,10 @@
     <div class="logo">Mon Logo</div>
     <nav>
         <ul>
-            <li><!--<a href="<?= SITE_URL; ?>/projets">-->Projets</a></li>
-            <li><!--<a href="<?= SITE_URL; ?>/equipements">-->Equipements</a></li>
-            <li><!--<a href="<?= SITE_URL; ?>/evenements">-->Evenements</a></li>
-            <li><!--<a href="<?= SITE_URL; ?>/histoire">-->Histoire</a></li>
+            <li><a href="<?= SITE_URL; ?>/projets">Projets</a></li>
+            <li><a href="<?= SITE_URL; ?>/equipements">Equipements</a></li>
+            <li><a href="<?= SITE_URL; ?>/evenements">Evenements</a></li>
+            <li><a href="<?= SITE_URL; ?>/histoire">Histoire</a></li>
         </ul>
     </nav>
 </header>
