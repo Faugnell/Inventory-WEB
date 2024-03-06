a3bb83fb9817fbdf907920f6a2c3e638ca6c952f
Faugnell <vic.petit98@gmail.com>
2024-03-06 16:19:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-// JavaScript pour gérer l'affichage du menu déroulant sous l'élément "Evenements"
-document.getElementById("evenements").addEventListener("mouseenter", function() {
-    document.getElementById("evenements-menu").style.display = "block";
-});
+document.addEventListener("DOMContentLoaded", function() {
+    // Votre code JavaScript ici
+    document.getElementById("evenements").addEventListener("mouseenter", function() {
+        document.getElementById("evenements-menu").style.display = "block";
+    });
 
-document.getElementById("evenements").addEventListener("mouseleave", function() {
-    document.getElementById("evenements-menu").style.display = "none";
+    document.getElementById("evenements").addEventListener("mouseleave", function() {
+        document.getElementById("evenements-menu").style.display = "none";
+    });
 });
