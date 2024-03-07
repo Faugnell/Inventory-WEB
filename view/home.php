93dc4b102426fe2f498a454f178077501146ef83
Faugnell <vic.petit98@gmail.com>
2024-03-07 16:03:40 +0100

    Update home.php

--- view/home.php
+++ view/home.php
@@ -5,7 +5,7 @@ require_once(realpath(__DIR__ . '/../view/part/header.php'));
         <!-- Premier bloc -->
         <div class="block first-block">
             <img src="/../assets/img/image_floutee.webp" alt="Image floutée">
-            <div class="overlay">
+            <div class="overlay" >
                 <h2>Bienvenue au FabLab</h2>
                 <p>Le Laboratoire de Fabrication (FabLab) est un espace unique qui offre l'accès à des outils de fabrication avancés et de prototypage. Nous proposons une variété de services incluant l'impression 3D, la découpe laser et l'usinage CNC. Notre équipe peut vous aider à concevoir et à réaliser votre prochain projet !</p>
                 <a href="histoire.html" class="button">Histoire</a>
@@ -18,31 +18,31 @@ require_once(realpath(__DIR__ . '/../view/part/header.php'));
             <p>Nous proposons une large gamme de services de fabrication pour vous aider à concrétiser vos idées. Que vous soyez étudiant, artiste, entrepreneur ou amateur, notre équipe d'experts peut vous aider à concevoir et à construire votre prochain projet. Visitez notre Lab'CESI dès aujourd'hui pour en savoir plus !</p>
             <div class="cards">
                 <!-- Card 1 -->
-                <div class="card">
+                <div class="card" href="">
                     <img src="/../assets/img/equipement1.jpg" alt="Equipement 1" href="">
                     <h3>Imprimante 3D</h3>
                     <p>Donnez vie à vos idées avec nos imprimantes 3D FDM et SLA.</p>
                 </div>
                 <!-- Card 2 -->
-                <div class="card">
+                <div class="card" href="">
                     <img src="/../assets/img/equipement2.jpg" alt="Equipement 2" href="">
                     <h3>Découpeuse Laser</h3>
                     <p>Créez des designs personnalisés avec nos découpeuses laser CO2 et à fibre.</p>
                 </div>
                 <!-- Card 3 -->
-                <div class="card">
+                <div class="card" href="">
                     <img src="/../assets/img/equipement3.jpg" alt="Equipement 3" href="">
                     <h3>CNC</h3>
                     <p>Produisez des pièces précises et des prototypes avec nos fraiseuses CNC.</p>
                 </div>
                 <!-- Card 4 -->
-                <div class="card">
+                <div class="card" href="">
                     <img src="/../assets/img/equipement4.jpg" alt="Equipement 4" href="">
                     <h3>Életroniques</h3>
                     <p>Construisez et réparez des circuits à l'aide de nos stations de soudage et de nos alimentations électriques.</p>
                 </div>
                 <!-- Card 5 -->
-                <div class="card">
+                <div class="card" href="">
                     <img src="/../assets/img/equipement5.jpg" alt="Equipement 5" href="">
                     <h3>Thermoformeuse</h3>
                     <p>Créez des moules en plastique et des emballages à l'aide de notre machine de thermoformage sous vide.</p>
